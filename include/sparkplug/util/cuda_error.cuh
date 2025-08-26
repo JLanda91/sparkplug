5e894eee7f87596f49a4a7413fc96b2a9c54b2d4
Jasper Landa <jasperlanda91@gmail.com>
2025-08-26 13:54:17 +0200

    Removed fmt dep and added license and notice

--- include/sparkplug/util/cuda_error.cuh
+++ include/sparkplug/util/cuda_error.cuh
@@ -4,9 +4,12 @@
 #include <string_view>
 
 namespace sparkplug::util {
-    struct cuda_error : std::runtime_error {
+    class cuda_error : public std::runtime_error {
+    public:
         cudaError_t code;
 
         explicit cuda_error(cudaError_t err, std::string_view msg = "CUDA Error");
+    private:
+        static std::string ToString(cudaError_t err, std::string_view msg);
     };
 }
--- src/cuda_error.cu
+++ src/cuda_error.cu
@@ -1,9 +1,16 @@
-#include <fmt/format.h>
+#include <sstream>
 
 #include "../include/sparkplug/util/cuda_error.cuh"
 
 namespace sparkplug::util {
     cuda_error::cuda_error(cudaError_t err, std::string_view msg)
-        : std::runtime_error(fmt::format("{}: {} (Error code: {})", msg, cudaGetErrorString(err), static_cast<std::underlying_type_t<cudaError_t>>(err)))
+        : std::runtime_error(ToString(err, msg))
         , code(err) {}
+
+    std::string cuda_error::ToString(cudaError_t err, std::string_view msg) {
+        std::ostringstream oss{};
+        oss << msg << ": " << cudaGetErrorString(err) << "(Error code: " << static_cast<std::underlying_type_t<cudaError_t>>(err) << ')';
+        return oss.str();
+    }
+
 }
